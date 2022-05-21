c4c095de6284b44af05b63c9cf438bcc098dfec7
Enoki <enokiun@gmail.com>
2022-05-21 14:25:12 +0300

    mmlol

--- migrations/20220503092423_users.sql
+++ migrations/20220503092423_users.sql
@@ -1,16 +1,20 @@
+# VARCHAR is used to represent an ID in most cases.
 CREATE TABLE IF NOT EXISTS users (
-	id VARCHAR(30) NOT NULL,
+	id VARCHAR NOT NULL,
 	username VARCHAR(32) NOT NULL,
 	display_name VARCHAR(32),
-	social_credit INT NOT NULL DEFAULT 0,
-	email VARCHAR(255) NOT NULL,
-	password VARCHAR(255) NOT NULL,
+        
+	# Thanks Emre, Olivier, Sharp Eyes and Sham.
+	social_credit INT NOT NULL DEFAULT 0, # All hail Xi Jinping
+	email VARCHAR(256) NOT NULL,
+	password VARCHAR(256) NOT NULL,
 	salt VARCHAR(32) NOT NULL,
-	status VARCHAR(255),
-	bio TEXT,
-	avatar VARCHAR(30) NOT NULL DEFAULT '0',
-	banner VARCHAR(30),
-	badges INTEGER NOT NULL DEFAULT 0,
-        permissions INTEGER NOT NULL DEFAULT 0,
+	status VARCHAR(256),
+	bio TEXT, # Should we uhhhhhh, limit this?
+	avatar VARCHAR NOT NULL DEFAULT '0',
+	banner VARCHAR,
+	badges INTEGER NOT NULL DEFAULT 0, # bitfield
+	permissions INTEGER NOT NULL DEFAULT 0, # bitfield
+	pubkey VARCHAR NOT NULL, # Not an ID (duh), blame olivier for this.
 	PRIMARY KEY (id)
 );
