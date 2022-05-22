8699918b61d6e32f8b73d27bd69e098110962743
Enoki <enokiun@gmail.com>
2022-05-22 20:32:36 +0300

    wow, explanation

--- src/main.rs
+++ src/main.rs
@@ -17,15 +17,18 @@ use sqlx::MySqlPool;
 use std::{collections::HashMap, env, net::SocketAddr, str::FromStr, sync::Arc};
 use tokio_tungstenite::{accept_async, tungstenite::Message, WebSocketStream};
 
+// A HashMap of addresses as keys and sinks as values in a Mutex that's in an Arc.
 type Peers = Arc<Mutex<HashMap<SocketAddr, SplitSink<WebSocketStream<TcpStream>, Message>>>>;
 type Database = Arc<Mutex<MySqlPool>>;
 
+// The only struct here, used in both json payloads and ws events.
 #[derive(Debug, Deserialize, Serialize, Clone)]
 struct ClientMessage {
     author: String,
     content: String,
 }
 
+// An example route that expects a payload of `ClientMessage` and broadcasts it to everyone connected to the websocket.
 #[post("/", format = "json", data = "<message>")]
 async fn index(state: &State<Peers>, message: Json<ClientMessage>) {
     let message = message.into_inner();
@@ -37,13 +40,16 @@ async fn index(state: &State<Peers>, message: Json<ClientMessage>) {
     }
 }
 
+// Showcasing a route communicating with the database.
 #[get("/test")]
 async fn test(state: &State<Database>) -> String {
     let db = state.lock().await;
     let res = sqlx::query!("SELECT * FROM users")
-        .fetch_all(&*db)
+        .fetch_all(&*db) // mmlol
         .await
         .unwrap();
+
+    // Probably shitty code
     let mut out = Vec::new();
     for r in res {
         if let Some(test) = r.username {
@@ -53,6 +59,7 @@ async fn test(state: &State<Database>) -> String {
     out.join("\n")
 }
 
+// A function that handles one peer connecting and disconnecting.
 async fn handle_connection(addr: SocketAddr, stream: TcpStream, peers: Peers) {
     let socket = accept_async(stream).await.unwrap();
 
@@ -78,6 +85,7 @@ async fn handle_connection(addr: SocketAddr, stream: TcpStream, peers: Peers) {
     }
 }
 
+// A function that starts the websocket and uses `handle_connection` for every peer.
 async fn handle_ws(state: Peers) {
     let socket = TcpListener::bind("0.0.0.0:8001").await.unwrap();
     println!("ws server started");
@@ -92,12 +100,15 @@ async fn handle_ws(state: Peers) {
 #[rocket::main]
 async fn main() {
     let state = Arc::new(Mutex::new(HashMap::new()));
+
+    // Establishing a DB connection.
     let pool = Arc::new(Mutex::new(
         MySqlPool::connect(&env::var("DATABASE_URL").unwrap())
             .await
             .unwrap(),
     ));
 
+    // Cors stuff
     let cors = CorsOptions {
         allowed_origins: AllowedOrigins::all(),
         allowed_methods: vec!["GET", "POST"]
@@ -109,8 +120,10 @@ async fn main() {
     .to_cors()
     .unwrap();
 
+    // The websocket task.
     task::spawn(handle_ws(state.clone()));
 
+    // The rest API.
     rocket::build()
         .mount("/", routes![index, test])
         .manage(state)
