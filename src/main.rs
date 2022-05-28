5f826589d79c4e65e6dc52dfb97f3ce9e6368a3e
Enoki <enokiun@gmail.com>
2022-05-28 23:30:57 +0300

    I forgor :skull:

--- src/main.rs
+++ src/main.rs
@@ -118,7 +118,7 @@ async fn main() -> Result<(), rocket::Error> {
     task::spawn(handle_ws(state.clone()));
 
     // The rest API.
-    let api = rocket::build()
+    rocket::build()
         .mount("/", routes![index])
         .manage(state)
         .manage(pool)
