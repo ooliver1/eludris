983fe291f8db0d77f30741b851438daf15139101
EnokiUN <enokiun@gmail.com>
2022-06-09 18:58:38 +0300

    chore: add verified field to user schema

--- migrations/20220503092423_users.sql
+++ migrations/20220503092423_users.sql
@@ -3,7 +3,7 @@ CREATE TABLE IF NOT EXISTS users (
 	id BIGINT NOT NULL,
 	username VARCHAR(32) NOT NULL,
 	display_name VARCHAR(32),
-        
+
 	# Thanks Emre, Olivier, Sharp Eyes and Sham.
 	social_credit INT NOT NULL DEFAULT 0, # All hail Xi Jinping
 	email VARCHAR(256) NOT NULL,
@@ -14,6 +14,7 @@ CREATE TABLE IF NOT EXISTS users (
 	avatar BIGINT,
 	banner BIGINT,
 	badges INTEGER NOT NULL DEFAULT 0, # bitfield
+	verified BOOLEAN NOT NULL DEFAULT FALSE,
 	permissions INTEGER NOT NULL DEFAULT 0, # bitfield
 	pubkey VARCHAR(9999) NOT NULL, # Not an ID (duh), blame olivier for this.
 	two_factor_auth VARCHAR(16),
