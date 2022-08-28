0fe353dce98de10a49f617ce54127b6359162681
Enoki <enokiun@gmail.com>
2022-08-28 20:01:29 +0300

    feat: Improved some gateway related stuff (#3)
    * feat: Improved some gateway related stuff
    
    * feat: Added a simple form of gateawy ratelimiting
    
    * fix: fix typo and make ratelimit reset a constant

--- src/gateway/mod.rs
+++ src/gateway/mod.rs
@@ -1,31 +1,30 @@
 //! The Eludris gateway, also called Pandemonium.
 
-use rocket::{
-    futures::{SinkExt, StreamExt},
-    tokio::{
-        net::{TcpListener, TcpStream},
-        select,
-        sync::Mutex,
-        task,
-        time::sleep,
-    },
-};
-use std::{env, net::SocketAddr, sync::Arc, time::Duration};
+use rocket::futures::{SinkExt, StreamExt};
+use rocket::tokio::net::{TcpListener, TcpStream};
+use rocket::tokio::{select, sync::Mutex, task, time};
+use std::time::{Duration, Instant};
+use std::{env, net::SocketAddr, sync::Arc};
 use tokio_tungstenite::{accept_async, tungstenite::Message};
 
-use crate::{
-    models::client::{Client, Clients},
-    utils::now_timestamp,
-};
+use crate::models::client::{Client, Clients};
 
-/// A simple function that check's if a client's last ping was over 20 seconds ago and closes the
-/// gateway connection if so.
-async fn check_connection(last_ping: Arc<Mutex<u32>>) {
+/// The duration it takes for a connection to be inactive in for it to be regarded as zombified and
+/// disconnected.
+const TIMEOUT_DURATION: Duration = Duration::from_secs(20);
+
+/// The minimum duration of time which can get a client disconnected for spamming gateway pings.
+const PING_RATELIMIT_RESET: Duration = Duration::from_secs(2);
+
+/// A simple function that check's if a client's last ping was over TIMEOUT_DURATION seconds ago and
+/// closes the gateway connection if so.
+async fn check_connection(last_ping: Arc<Mutex<Instant>>) {
+    let mut interval = time::interval(TIMEOUT_DURATION);
     loop {
-        if (*last_ping.lock().await + 20) < now_timestamp() {
+        if Instant::now().duration_since(*last_ping.lock().await) > TIMEOUT_DURATION {
             break;
         }
-        sleep(Duration::from_secs(20)).await;
+        interval.tick().await;
     }
 }
 
@@ -38,7 +37,7 @@ async fn handle_connection(addr: SocketAddr, stream: TcpStream, clients: Clients
     let (tx, mut rx) = socket.split();
     let tx = Arc::new(Mutex::new(tx));
 
-    let last_ping = Arc::new(Mutex::new(now_timestamp()));
+    let last_ping = Arc::new(Mutex::new(Instant::now()));
 
     {
         let mut clients = clients.lock().await;
@@ -54,14 +53,20 @@ async fn handle_connection(addr: SocketAddr, stream: TcpStream, clients: Clients
             match msg {
                 Ok(data) => match data {
                     Message::Ping(x) => {
-                        *last_ping.lock().await = now_timestamp();
+                        let mut last_ping = last_ping.lock().await;
+                        if Instant::now().duration_since(*last_ping) < PING_RATELIMIT_RESET {
+                            // A simple form of gateway ratelimiting.
+                            log::info!("Disconnected a client: {}, reason: Ping ratelimit", addr);
+                            break;
+                        }
+                        *last_ping = Instant::now();
                         tx.lock()
                             .await
                             .send(Message::Pong(x))
                             .await
                             .expect("Couldn't send pong");
                     }
-                    _ => todo!(),
+                    _ => log::debug!("Unsupported Gateway message type."),
                 },
                 Err(_) => break,
             }
@@ -91,8 +96,8 @@ async fn handle_connection(addr: SocketAddr, stream: TcpStream, clients: Clients
 pub async fn start(clients: Clients) {
     let gateway_address = format!(
         "{}:{}",
-        env::var("gateway_ADDRESS").unwrap_or_else(|_| "127.0.0.1".to_string()),
-        env::var("gateway_PORT").unwrap_or_else(|_| "5000".to_string())
+        env::var("GATEWAY_ADDRESS").unwrap_or_else(|_| "127.0.0.1".to_string()),
+        env::var("GATEWAY_PORT").unwrap_or_else(|_| "9000".to_string())
     );
     let socket = TcpListener::bind(&gateway_address)
         .await
