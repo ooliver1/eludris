5469a6bc80a1a604e22f6fccc9c2fbe92cc68e96
ooliver1 <oliverwilkes2006@icloud.com>
2022-12-12 22:30:39 +0000

    tmp

--- oprish/src/main.rs
+++ oprish/src/main.rs
@@ -8,13 +8,100 @@ mod cors;
 mod ratelimit;
 mod routes;
 
-use std::env;
+use std::{alloc::System, env, ffi::OsStr, io, rc::Rc};
 
 use anyhow::Context;
+use libloading::Library;
 use rocket::{Build, Config, Rocket};
 use rocket_db_pools::Database;
 use routes::*;
 use todel::Conf;
+
+use plugins::{Plugin, RequestListener};
+
+#[global_allocator]
+static ALLOCATOR: System = System;
+
+struct PluginRegistrar {
+    callbacks: Vec<RequestListenerProxy>,
+    lib: Rc<Library>,
+}
+
+impl PluginRegistrar {
+    fn new(lib: Rc<Library>) -> PluginRegistrar {
+        PluginRegistrar {
+            lib,
+            callbacks: Vec::default(),
+        }
+    }
+}
+
+impl plugins::PluginRegistrar for PluginRegistrar {
+    fn register_request_listener(&mut self, callback: Box<dyn RequestListener>) {
+        let proxy = RequestListenerProxy {
+            callback,
+            _lib: Rc::clone(&self.lib),
+        };
+        self.callbacks.push(proxy);
+    }
+}
+
+#[derive(Default)]
+pub struct RequestListeners {
+    callbacks: Vec<RequestListenerProxy>,
+    libraries: Vec<Rc<Library>>,
+}
+
+impl RequestListeners {
+    pub fn new() -> RequestListeners {
+        RequestListeners::default()
+    }
+
+    /// # Safety
+    ///
+    /// A plugin **must** be implemented using the
+    /// [`plugins::export_plugin!()`] macro. Trying to manually implement
+    /// a plugin without going through that macro will result in undefined
+    /// behaviour.
+    pub unsafe fn load<P: AsRef<OsStr>>(
+        &mut self,
+        library_path: P,
+    ) -> Result<(), Box<dyn std::error::Error>> {
+        let library = Rc::new(Library::new(library_path)?);
+
+        let plugin = library.get::<*mut Plugin>(b"plugin\0")?.read();
+
+        // version checks to prevent accidental ABI incompatibilities
+        if plugin.rustc_version != plugins::RUSTC_VERSION
+            || plugin.core_version != plugins::CORE_VERSION
+        {
+            return Err(Box::new(io::Error::new(
+                io::ErrorKind::Other,
+                "Version mismatch",
+            )));
+        }
+
+        let mut registrar = PluginRegistrar::new(Rc::clone(&library));
+
+        (plugin.register)(&mut registrar);
+
+        self.callbacks.extend(registrar.callbacks);
+        self.libraries.push(library);
+
+        Ok(())
+    }
+}
+
+pub struct RequestListenerProxy {
+    callback: Box<dyn RequestListener>,
+    _lib: Rc<Library>,
+}
+
+impl RequestListener for RequestListenerProxy {
+    fn call(&self, request: rocket::Request) {
+        self.callback.call(request)
+    }
+}
 
 #[derive(Database)]
 #[database("cache")]
