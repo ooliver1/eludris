6baa9b81ca4e8246356c63e3cf704453f9162c4b
Enoki <enokiun@gmail.com>
2023-02-20 14:56:43 +0300

    chore: remove remnants of rate_limits

--- todel/src/models/mod.rs
+++ todel/src/models/mod.rs
@@ -4,12 +4,10 @@ mod files;
 mod gateway;
 mod info;
 mod messages;
-mod rate_limits;
 mod response;
 
 pub use files::*;
 pub use gateway::*;
 pub use info::*;
 pub use messages::*;
-pub use rate_limits::*;
 pub use response::*;
