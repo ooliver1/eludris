57b3f15450ce6fbd84d1c70c0e8cd00eb8ae0602
Enoki <enokiun@gmail.com>
2023-02-20 15:07:50 +0300

    Revert "feat: move /ratelimits route into the / route"

--- todel/src/models/mod.rs
+++ todel/src/models/mod.rs
@@ -4,10 +4,12 @@ mod files;
 mod gateway;
 mod info;
 mod messages;
+mod rate_limits;
 mod response;
 
 pub use files::*;
 pub use gateway::*;
 pub use info::*;
 pub use messages::*;
+pub use rate_limits::*;
 pub use response::*;
